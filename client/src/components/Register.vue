b91d9ed0fb462ce04109a4119c6703c75f3764a8
icebeam030 <icebeam030@outlook.com>
2018-09-13 18:22:49 +1000

    Build registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="register">
+    <h1>Register</h1>
+    <input v-model="email" type="email" name="email" placeholder="Email" />
+    <br>
+    <input v-model="password" type="password" name="password" placeholder="Password" />
+    <br>
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import authenticationService from '@/services/authenticationService'
+
+export default {
+  name: 'Register',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await authenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Home from './views/Home.vue'
+import Register from './components/Register.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '/register',
+      name: 'register',
+      component: Register
     },
     {
       path: '/about',
