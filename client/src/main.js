df33dad8b33d85320e81f592ff7f638358ae5ce8
icebeam030 <icebeam030@outlook.com>
2019-10-17 21:26:55 +1100

    Dependencies clear-up

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,8 @@
-import '@babel/polyfill'
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
